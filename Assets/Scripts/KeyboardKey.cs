13fadfb09996e975a3e7e34e50ca26398ce6d9db
furkancanc <fcc.ceylan@gmail.com>
2025-01-19 18:56:41 +0300

    KeyboardKey Event

--- Assets/Scripts/KeyboardKey.cs
+++ Assets/Scripts/KeyboardKey.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class KeyboardKey : MonoBehaviour
 {
     [Header(" Elements ")]
     [SerializeField] private TextMeshProUGUI letterText;
 
+    [Header(" Events ")]
+    public static Action<char> onKeyPressed;
+
     private void Start()
     {
-        GetComponent<Button>().onClick.AddListener(Test);
+        GetComponent<Button>().onClick.AddListener(SendKeyPressedEvent);
     }
 
-    private void Test()
+    private void SendKeyPressedEvent()
     {
-        Debug.Log(letterText.text);
+        onKeyPressed?.Invoke(letterText.text[0]);
     }
 }
