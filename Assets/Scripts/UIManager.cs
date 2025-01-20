1492cc886af6928254df104709c82c7bd9f316bc
furkancanc <fcc.ceylan@gmail.com>
2025-01-20 23:01:18 +0300

    Creating The UI Manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [Header(" Elements ")]
+    [SerializeField] private CanvasGroup gameCG;
+    [SerializeField] private CanvasGroup levelCompleteCG;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShowGame()
+    {
+        ShowCG(gameCG);
+    }
+
+    private void HideGame()
+    {
+        HideCG(gameCG);
+    }
+
+    private void ShowLevelComplete()
+    {
+        ShowCG(levelCompleteCG);
+    }
+
+    private void HideLevelComplete()
+    {
+        HideCG(levelCompleteCG);
+    }
+
+    private void ShowCG(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+
+    private void HideCG(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+}
