5282baec6d20b535fe86d6e527fec150ebd3abe4
furkancanc <fcc.ceylan@gmail.com>
2025-01-20 22:13:09 +0300

    Fixing the Keyboard Colors

--- Assets/Scripts/KeyboardKey.cs
+++ Assets/Scripts/KeyboardKey.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 
+enum Validity { None, Valid, Potential, Invalid }
+
 public class KeyboardKey : MonoBehaviour
 {
     [Header(" Elements ")]
     [SerializeField] private Image renderer;
     [SerializeField] private TextMeshProUGUI letterText;
+
+    [Header(" Settings ")]
+    private Validity validity;
 
     [Header(" Events ")]
     public static Action<char> onKeyPressed;
@@ -17,6 +22,13 @@ public class KeyboardKey : MonoBehaviour
     private void Start()
     {
         GetComponent<Button>().onClick.AddListener(SendKeyPressedEvent);
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        renderer.color = Color.white;
+        validity = Validity.None;
     }
 
     private void SendKeyPressedEvent()
@@ -31,16 +43,34 @@ public class KeyboardKey : MonoBehaviour
 
     public void SetValid()
     {
+        if (validity == Validity.Valid)
+        {
+            return;
+        }
+
         renderer.color = Color.green;
+        validity = Validity.Valid;
     }
 
     public void SetPotential()
     {
+        if (validity == Validity.Valid)
+        {
+            return;
+        }
+
         renderer.color = Color.yellow;
+        validity = Validity.Potential;
     }
 
     public void SetInvalid()
     {
+        if (validity == Validity.Valid || validity == Validity.Potential)
+        {
+            return;
+        }
+
         renderer.color = Color.gray;
+        validity = Validity.Invalid;
     }
 }
