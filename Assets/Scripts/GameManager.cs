6d313679e3497682a060da33f5786309bd031036
furkancanc <fcc.ceylan@gmail.com>
2025-01-20 22:20:27 +0300

    Creating the Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { Menu, Game, LevelComplete, GameOver, Idle}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [Header(" Settings ")]
+    private GameState gameState;
+
+    [Header(" Events ")]
+    public static Action<GameState> onGameStateChanged;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetGameState(GameState gameState)
+    {
+        this.gameState = gameState;
+        onGameStateChanged?.Invoke(gameState);
+    }
+}
